5654cfd26fdb72f31987dc9b572c3ddd399bb8e8
sanemat <o.gata.ken@gmail.com>
2013-06-16 03:52:07 +0900

    Add identities#new

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :identity,
-    on_login: ->(env) { SessionsController.action(:new).call(env) }
+    on_login: ->(env) { SessionsController.action(:new).call(env) },
+    on_registration: ->(env) { IdentitiesController.action(:new).call(env) }
 end
 OmniAuth.config.logger = Rails.logger
