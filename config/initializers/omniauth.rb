e56997a2d1eb4c31a876e09afe10e0f92adb8e5e
sanemat <o.gata.ken@gmail.com>
2013-06-16 03:38:31 +0900

    Remove unnessesary brace

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :identity, {
+  provider :identity,
     on_login: ->(env) { SessionsController.action(:new).call(env) }
-  }
 end
 OmniAuth.config.logger = Rails.logger
