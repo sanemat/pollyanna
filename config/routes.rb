067fdb78b3264c8caca53cd8c25e3cc579f82142
sanemat <o.gata.ken@gmail.com>
2013-06-15 22:28:28 +0900

    Change to single quote

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "help/detail"
-  get "session/create"
+  get 'help/detail'
+  get 'session/create'
   root 'help#detail'
   resources :messages
   # The priority is based upon order of creation: first created -> highest priority.
