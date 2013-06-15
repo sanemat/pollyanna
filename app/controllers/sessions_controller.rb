59e4a5a8777640ad58ca8544ed15cd0d19d242ee
sanemat <o.gata.ken@gmail.com>
2013-06-16 01:32:21 +0900

    Use path instead of url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to messages_url, notice: "Signed in!"
+    redirect_to messages_path, notice: "Signed in!"
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Signed out!"
+    redirect_to root_path, notice: "Signed out!"
   end
 
   def failure
-    redirect_to root_url, alert: "Authentication failed, please try again."
+    redirect_to root_path, alert: "Authentication failed, please try again."
   end
 end
