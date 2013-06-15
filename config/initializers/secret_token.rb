8a7827ad16356148649a7438433da51d3d9351df
sanemat <o.gata.ken@gmail.com>
2013-06-15 14:37:06 +0900

    Initialize rails
    $ bundle exec rails new . -T -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails.application.config.secret_key_base = '9764c1eb02224c54e1a0b006c65f7483362f133818b9d522654bba78b6ab8fcf2aa96cbd5ab8b5db9a10351cc82e4bee641f4cd1b6b8a37fbb24e62730a9a6bf'
