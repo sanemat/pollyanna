ed0ef9e2c16c9e8637ea47c38d3e51aecb86e698
sanemat <o.gata.ken@gmail.com>
2013-06-15 23:48:29 +0900

    Find or create user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,17 @@ class SessionsController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to messages_url, notice: "Signed in!"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Signed out!"
+  end
+
+  def failure
+    redirect_to root_url, alert: "Authentication failed, please try again."
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   rolify
+
+  def self.from_omniauth(auth)
+    find_by(provider: auth['provider'], uid: auth['uid']) || create_with_omniauth(auth)
+  end
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      user.name = auth['info']['name']
+    end
+  end
 end
